08579b2b41aa5369f2a5f8991e804367c081b4fe
tree12132002 <tree12132002@hotmail.com>
2021-08-29 06:28:44 +0800

    feat: modify index.hbs

--- models/CATEGORY.js
+++ models/CATEGORY.js
@@ -1,6 +1,6 @@
 const CATEGORY = {
   home: 'fas fa- home',
-  transporttation: 'fas fa-shuttle-van',
+  transportation: 'fas fa-shuttle-van',
   entertainment: 'fas fa-grin-beam',
   food: 'fas fa-utensils',
   other: 'fas fa-pen'
