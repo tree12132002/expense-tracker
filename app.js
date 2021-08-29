a004240d952961ed22d156463b2ac17ccd561409
tree12132002 <tree12132002@hotmail.com>
2021-08-29 09:09:05 +0800

    feat: add update function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -37,6 +37,31 @@ app.get('/expenses/new', (req, res) => {
 app.post('/expenses', (req, res) => {
   const { name, date, category, amount } = req.body
   return Expense.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+// edit record
+app.get('/expenses/:id/edit', (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  return Expense.findById(id)
+    .lean()
+    .then((expense) => res.render('edit', { expense }))
+    .catch(error => console.error(error))
+})
+
+app.post('/expenses/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  return Expense.findById(id)
+    .then(expense => {
+      expense.name = name
+      expense.date = date
+      expense.category = category
+      expense.amount = amount
+      return expense.save()
+    })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
