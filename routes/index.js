13e734920f1051b965ec6a474840b2b4d820490e
yourname <tree12132002@hotmail.com>
2021-10-29 03:53:37 +0800

    feat: add user authentication in routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const expenses = require('./modules/expenses')
 const category = require('./modules/category')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/expenses', expenses)
-router.use('/category', category)
+router.use('/expenses', authenticator, expenses)
+router.use('/category', authenticator, category)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
