2c5271cc85474d09665371a6babd9a623b0555f7
yourname <tree12132002@hotmail.com>
2021-10-29 01:55:31 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// enter login page
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// login function
+router.post('/login', (req, res) => {
+  
+})
+
+// enter register page
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
