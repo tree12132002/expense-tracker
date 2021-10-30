4fba56a2cdc70f0d5eae77087bb5ff47e7e6377b
yourname <tree12132002@hotmail.com>
2021-10-31 06:03:49 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
@@ -24,10 +25,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.errors_msg = req.flash('errors_msg') // 設定 errors_msg 訊息
   next()
 })
 
--- config/passport.js
+++ config/passport.js
@@ -7,14 +7,14 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('errors_msg', '此信箱尚未註冊！'))
         }
         if (password !== user.password) {
-          return done(null, false, { message: 'Email or Password is incorrect!' })
+          return done(null, false, req.flash('errors_msg', '密碼錯誤！'))
         }
         return done(null, user)
       })
